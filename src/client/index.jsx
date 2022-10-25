fab96a85578322695fe202e121e230076b342f0a
mfouquet <manon.fouquet@artelys.com>
2022-10-25 22:56:24 +0200

    introduce styles

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import TodoList from './components/TodoList';
 
-//document.getElementById('app').textContent="TEST"
-ReactDOM.render(<TodoList />, document.getElementById('app'));
+// Styles
+import "./styles/main.scss";
+
+const root = createRoot(document.getElementById('app'));
+root.render(<TodoList />);
