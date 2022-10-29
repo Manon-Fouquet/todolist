b53962bf49b5477a84d8d82e3473965fb347488d
mfouquet <manon.fouquet@artelys.com>
2022-10-29 21:29:37 +0200

    initialize db

--- src/server/todo_server/todo_api/models.py
+++ src/server/todo_server/todo_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    descr = models.CharField(max_length=256)
+    completed = models.BooleanField()
+
+    def __str__(self):
+        return f'Task #{self.id} : {self.descr} ({"completed" if self.completed else "Todo"})'
