60c351d4b788a4ae2bf940de42e5499445d9012f
mfouquet <manon.fouquet@artelys.com>
2022-10-30 17:15:25 +0100

    line through when task done

--- src/client/components/TaskItem.jsx
+++ src/client/components/TaskItem.jsx
@@ -20,9 +20,12 @@ export default class TaskItem extends React.Component{
         return (
             <div id={"task-"+this.props.id} className="task flex-row">
                 <input className='task-check-box' type="checkbox" id={"checked-"+this.props.id}  defaultChecked = {this.props.completed} onChange={this.switch} />
-                <label className='task-descr text-regular-default' htmlFor={this.props.id} id={'task-descr-'+this.props.id}>
+                {this.state.completed?<label className='task-descr text-regular-default striked' htmlFor={this.props.id} id={'task-descr-'+this.props.id}>
                    {this.props.descr} 
-                </label>
+                </label>:
+                <label className='task-descr text-regular-default' htmlFor={this.props.id} id={'task-descr-'+this.props.id}>
+                {this.props.descr} 
+             </label>}
                 <img className="task-delete small-circle-icon" src={TrashIcon}  id={"delete-"+this.props.id} onClick = {()=>this.props.deleteMe(this.props.id)}/>
             </div>
         );
